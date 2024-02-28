34e0d065b83f8d489d446915c198386d84cfa100
Sydney Sharp <sydney.marie.sharp@gmail.com>
2024-02-28 11:35:19 -0600

    updating

---
+++ fishingBoat.js
@@ -0,0 +1,61 @@
+//const fishArray
+
+// [
+//     { id: 1, species: "Halibut", weight: 6, price: 3.55, amount: 30 },
+//     { id: 2, species: "Mackerel", weight: 3, price: 4.10, amount: 48 },
+//     { id: 3, species: "Salmon", weight: 5, price: 3.05, amount: 25 },
+//     { id: 4, species: "Walleye", weight: 2, price: 9.45, amount: 19 },
+//     { id: 5, species: "Sunfish", weight: 25, price: 5.18, amount: 1 },
+//     { id: 6, species: "Orange Roughy", weight: 4, price: 6.95, amount: 37 },
+//     { id: 7, species: "Tuna", weight: 18, price: 8.66, amount: 5 },
+//     { id: 8, species: "Puffer", weight: 2, price: 9.84, amount: 52 }
+// ]
+
+
+/*
+ENGLISH:
+
+Data structure will be an array of objects named fish array
+Here are the properties of each fish.
+    -Species
+    -Weight
+    -Price (prices range from $3.00 to $10.00 per fish)
+    -Amount (how many of this species were caught today)
+
+Define a function that the other modules can invoke in order to see today's catch.
+The function will be called boatInventory. 
+When the function is invoked it will return the array of fish objects 
+
+CODEENGLISH:
+
+const boatInventory = (item in the array)=> {
+-Species
+    -Weight
+    -Price 
+    -Amount
+return  daily catch
+}
+
+
+( go back and compair notes for the bug rangler)
+
+-we will need to use reqire and export to connect this function to the other modules
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+export {boatInventory};
+
+
+
+
+
